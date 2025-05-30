5188a2ac64acd0c160ac1b5d279c773c5d03a649
voyager36831 <dje.voyager3@gmail.com>
2025-05-30 22:52:38 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function playSound1() {
+      const audio = document.getElementById("anotherballsupjohnnysound");
+      audio.play();
+    }
+
+
+function playSound2() {
+      const audio = document.getElementById("giveme24hourssound");
+      audio.play();
+    }
+
+function playSound3() {
+      const audio = document.getElementById("iamoverheresound");
+      audio.play();
+    }
+
+function playSound4() {
+      const audio = document.getElementById("masteramireadysound");
+      audio.play();
+    }
+
+function playSound5() {
+      const audio = document.getElementById("mrpussycatsound");
+      audio.play();
+    }
+
+function playSound6() {
+      const audio = document.getElementById("yourgrandmotherissicksound");
+      audio.play();
+    }
+ 
